f3113145ebb5924dd7dd7ead717bb84d750c700f
Zack Kollar <rallokkcaz@gmail.com>
2018-11-29 05:34:39 -0800

    It's lookin good

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,30 @@
-<template>
-  <div class="home">
-    <div class="first-panel">
-      <delay :duration="1000">
-        <fade-transition>
-          <hero />
-        </fade-transition>
-      </delay>
-    </div>
-    <div class="second-panel">
-
-    </div>
+<template lang="pug">
+  .home
+    .first-panel
+      delay(:duration="1000")
+        fade-transition
+          hero
+    .page-container
+      .second-panel.container
+        .card
+          img(src="~@/assets/images/icons/noun_skills.svg")
+          .card__title Innovation
+          .card__copy 
+            | Innovative designs and development practices to keep up with not only current trends
+            | but up and coming technologies & services.
+        .card
+          img(src="~@/assets/images/icons/noun_respectful.svg")
+          .card__title Collaboration
+          .card__copy 
+            | Our team will work with you one on one to create the best website for your needs,
+            | while also keeping your budget. Our in house designers and developers are personable
+            | and are always willing to go above and beyond to meet your style.
+        .card
+          img(src="~@/assets/images/icons/noun_runes.svg")
+          .card__title Future Proof
+          .card__copy 
+            | Staying ahead of the curve in 2018 is no easy task,
+            | luckily we've been building user experiences for half a decade.
   </div>
 </template>
 
@@ -28,10 +43,18 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
   @import '~@/styles/_globals.scss';
+  @import '~@/styles/_typography.scss';
 
   .home {
     background: $brand-color-purple;
     min-height: 100vh;
+  }
+
+  .first-panel, .second-panel {
+    min-height: 50vh;
+    justify-content: center;
+    align-items: center;
+    display: flex;
   }
 
   .first-panel {
@@ -45,16 +68,76 @@ export default Vue.extend({
     *::selection { color: blue !important; }
     *::-moz-selection { color: blue !important; }
 
-
-    min-height: 50vh;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-
-
-
     @media only screen and (max-width: $mobile-size) {
       min-height: 115vw;
+    }
+  }
+
+  .second-panel {
+    // display: grid;
+    // grid-template-columns: repeat(3, 1fr);
+    // grid-gap: 2rem;
+    padding: 2rem 0;
+
+    @media only screen and (max-width: $tablet-size) {
+      grid-gap: 0;
+    }
+
+    @media only screen and (max-width: $mobile-size) {
+      display: block;
+      padding: 3rem 0;
+    }
+  }
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+    color: white;
+
+    padding: 2rem;
+    height: 100%;
+
+    img {
+      width: 60%;
+      fill: white;
+      max-width: 154px;
+
+      @media only screen and (max-width: $mobile-size) {  
+
+      }
+      @media only screen and (max-width: $mobile-size) {  
+        width: 40%;
+      }
+    }
+
+    &__title {
+      font-weight: bold;
+      letter-spacing: 1px;
+      padding-bottom: 2px;
+
+      border-bottom: 2px solid white;
+      font-family: "Now", sans-serif;
+      text-transform: uppercase;
+
+      
+      margin-bottom: 2rem;
+    }
+
+    &__copy {
+      text-align: center;
+      letter-spacing: 0.25px;
+      line-height: 1.25;
+
+      @include fluid-type(13px, 16px);
+      // font-family: "Now", sans-serif;
+    }
+
+    @media only screen and (max-width: $mobile-size) {
+      height: inherit;
+      margin-bottom: 2rem;
     }
   }
 </style>
