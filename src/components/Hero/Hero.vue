1a6d2531023f8fcd34adc1a9844f5b87d1618819
Zack Kollar <rallokkcaz@gmail.com>
2018-11-30 17:59:07 -0800

    Tune ups and minor graphical fixes

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -13,7 +13,7 @@ export default Vue.extend({
     .hero
       zoom-x-transition(:duration=600 appear)
         .hero__heading Minimalist Design
-      delay(:duration=700 placeholderHeight="3rem")
+      delay(:duration=700 placeholderHeight="3em")
         slide-x-left-transition(:duration=700 appear)
           .hero__message Creating simplistic and feature rich web applications for the 22nd Century
 </template>
