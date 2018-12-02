99690e7f859542652b9b314ab531c53ae5331350
Zack Kollar <rallokkcaz@gmail.com>
2018-12-02 02:22:32 -0800

    PeacE

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -10,12 +10,12 @@ export default Vue.extend({
 </script>
 
 <template lang="pug">
-    .hero
-      zoom-x-transition(:duration=600 appear)
-        .hero__heading Minimalist Design
-      delay(:duration=700 placeholderHeight="3em")
-        slide-x-left-transition(:duration=700 appear)
-          .hero__message Creating simplistic and feature rich web applications for the 22nd Century
+.hero
+  zoom-x-transition(:duration=600 appear)
+    .hero__heading Minimalist Design
+  delay(:duration=700 placeholderHeight="3em")
+    slide-x-left-transition(:duration=700 appear)
+      .hero__message Creating simplistic and feature rich web applications for the 22nd Century
 </template>
 
 <style scoped lang="scss" src="@/components/Hero/Hero.scss"></style>
